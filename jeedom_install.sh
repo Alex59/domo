a58c26f79817daecc7e42e37af6f1f6084c4c661
Alex59 <alexis.richez@free.fr>
2017-09-16 14:13:57 +0200

    Update jeedom_install.sh
    Sources for installation script added

--- jeedom_install.sh
+++ jeedom_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#############################################################################################
+# Jeedom installation script
+# Sources :
+# https://guillaumebraillon.fr/jeedom-installation-de-raspbian-et-jeedom-sur-raspberry/
+# https://jeedom.github.io/documentation/installation/fr_FR/#_autre_diy
+#############################################################################################
 echo "Installing Jeedom..."
 wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh
 chmod +x install.sh
